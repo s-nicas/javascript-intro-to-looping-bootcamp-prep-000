6ca2ae0ea62ac5dcb9a0854d058352006dce4517
Sonia  D <sduarte22@gmail.com>
2018-03-02 19:49:17 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -23,3 +23,14 @@ function whileLoop(number){
     
 }
 
+function doWhileLoop(array){
+function maybeTrue() {
+  return Math.random() >= 0.5;
+}
+do{
+  array.pop();
+}
+while (array.length > 0 && maybeTrue())
+return array ;
+}
+
